394bedc51b621d0f3a9a6c04d41db86660667028
mark1xie <54355534453@qq.com>
2017-05-09 12:34:25 +0800

    add

---
+++ tools/livtripmanager-common/src/main/java/com/qccr/livtrip/common/util/XStreamUtil.java
@@ -0,0 +1,37 @@
+package com.qccr.livtrip.common.util;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
+/**
+ * Created by xierongli on 17/5/9.
+ */
+public class XStreamUtil {
+
+
+    public static String convertObjectToXml(Class<?> classInstance){
+        JAXBContext context;
+        try {
+            context = JAXBContext.newInstance(classInstance);
+            Marshaller mar = context.createMarshaller();
+            mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            mar.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+
+            StringWriter writer = new StringWriter();
+
+            try {
+                mar.marshal(classInstance.newInstance(), writer);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(writer.toString());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+}
